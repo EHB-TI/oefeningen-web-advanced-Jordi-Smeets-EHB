c6027f4848ba116b8815abea6779ab59cedf20f9
Swarly92 <jordi.smeets@student.ehb.be>
2022-06-05 00:27:14 +0200

    JavaScript basics doornemen

---
+++ Oef 1/js/script.js
@@ -0,0 +1,17 @@
+'use-strict'
+
+//1.1
+alert("Hello World")
+
+//1.2
+let a = 10;
+a=a + 5;
+console.log(a);
+
+//1.3
+let age =prompt();
+if (age > 18) {
+    console.log("Welkom!");
+  }
+
+//1.4
