f54fd4c055149545c333b2b115809e3ba5372c20
Swarly92 <jordi.smeets@student.ehb.be>
2022-06-06 00:04:38 +0200

    oef 1.8

--- Oef 1/js/script.js
+++ Oef 1/js/script.js
@@ -21,16 +21,45 @@ if (age > 18) {
 
 //Oef 1.4
 
+document.getElementById("formOef1").addEventListener('submit',checkAge);
   
-  document.getElementById("formOef1").addEventListener('submit',checkAge);
-    
-  function checkAge(event){
-    event.preventDefault();
-    
-    const age= document.getElementById("age").value;
-    console.log(age)
-    if (age > 18) {
-      const name= document.getElementById("name").value;
-      console.log(`${name}, je bent ouder dan 18 jaar!`);
-    }
+function checkAge(event){
+  event.preventDefault();
+  
+  const age= document.getElementById("age").value;
+  console.log(age)
+
+  if (age > 18) {
+    const name= document.getElementById("name").value;
+    console.log(`${name}, je bent ouder dan 18 jaar!`);
   }
+}
+
+//oef 1.7 + 1.8
+
+function capitalize(name){ 
+
+  let names=name.trim().split(" ");
+  let capitalNames = new Array;
+
+  /*
+  for(let i in names)
+  {
+    name= names[i]
+    capitalNames[i]= name.charAt(0).toUpperCase() + name.substring(1);
+  }
+  */
+
+  for(let name of names)
+  {
+    capitalNames.push(name.charAt(0).toUpperCase() + name.substring(1));
+  }
+
+  console.log(capitalNames.join(" "));
+  
+}
+
+capitalize("smeets jordi");
+capitalize("mike derycke");
+capitalize("   max power    ");
+capitalize(" judas de verrader");
