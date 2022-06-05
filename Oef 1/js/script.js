6600d3a9c562fc36fe77f78cc1d6bb54bd372036
Swarly92 <jordi.smeets@student.ehb.be>
2022-06-05 23:23:58 +0200

    oef 1.4

--- Oef 1/js/script.js
+++ Oef 1/js/script.js
@@ -1,17 +1,36 @@
 'use-strict'
 
-//1.1
-alert("Hello World")
+//Oef 1.1
+//alert("Hello World")
 
-//1.2
+
+/* Oef 1.2
+
 let a = 10;
 a=a + 5;
 console.log(a);
+*/
 
-//1.3
+/* Oef 1.3
+
 let age =prompt();
 if (age > 18) {
     console.log("Welkom!");
   }
+*/
 
-//1.4
+//Oef 1.4
+
+  
+  document.getElementById("formOef1").addEventListener('submit',checkAge);
+    
+  function checkAge(event){
+    event.preventDefault();
+    
+    const age= document.getElementById("age").value;
+    console.log(age)
+    if (age > 18) {
+      const name= document.getElementById("name").value;
+      console.log(`${name}, je bent ouder dan 18 jaar!`);
+    }
+  }
